72cc8ffac93c7f301ae4fb72a0d1c8c6f8839df7
Productivity <grantchen2021@gmail.com>
2024-06-22 19:46:50 -0400

    implemented CodeBert

--- vector_embedder_service/main.py
+++ vector_embedder_service/main.py
@@ -1,12 +1,14 @@
 import os
     
-from vector_embedder_service import config, server
+from vector_embedder_service import config, server, utils
 
 
 def main() -> None:
     env = os.environ.get("ENV")
     config.load_env_vars(env)
     print(f"ENV={env}")
+    
+    utils.CodeBert.initialize()
 
     server.start()
     
--- vector_embedder_service/utils/code_bert.py
+++ vector_embedder_service/utils/code_bert.py
@@ -1,5 +1,33 @@
+import torch
+
+from transformers import RobertaTokenizer, RobertaModel
+
+
 class CodeBert:
-    @staticmethod
-    def vector_embed_source_code(source_code: str) -> list:
-        # TODO
-        return []
+    _tokenizer = None
+    _model = None
+
+    @classmethod
+    def initialize(cls) -> None:
+        cls._tokenizer = RobertaTokenizer.from_pretrained("microsoft/codebert-base")
+        cls._model = RobertaModel.from_pretrained("microsoft/codebert-base")
+
+    @classmethod
+    def vector_embed_source_code(cls, source_code: str) -> list[float]:
+
+        # Tokenize the input source code
+        inputs = cls._tokenizer(
+            source_code, return_tensors="pt", truncation=True, padding=True
+        )
+
+        # Get the embeddings
+        with torch.no_grad():
+            outputs = cls._model(**inputs)
+
+        # The embeddings are in the last hidden state
+        embeddings = outputs.last_hidden_state
+
+        # Pool the embeddings (e.g., take the mean of all tokens' embeddings)
+        embeddings = embeddings.mean(dim=1).squeeze()
+
+        return embeddings.tolist()
