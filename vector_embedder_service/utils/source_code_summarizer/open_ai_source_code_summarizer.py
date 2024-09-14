b14eac477d833e286f8de14b11be79102f3e2915
Productivity <grantchen2021@gmail.com>
2024-09-14 15:15:24 -0400

    fixed bugs in OpenAiSourceCodeSummarizer, still buggy

--- vector_embedder_service/utils/source_code_summarizer/open_ai_source_code_summarizer.py
+++ vector_embedder_service/utils/source_code_summarizer/open_ai_source_code_summarizer.py
@@ -1,4 +1,4 @@
-import json, openai, os, time, uuid
+import json, openai, os, tiktoken, time, uuid
 from concurrent.futures import ThreadPoolExecutor
 
 from .base_source_code_summarizer import BaseSourceCodeSummarizer
@@ -6,14 +6,17 @@ from vector_embedder_service import utils
 
 
 class OpenAiSourceCodeSummarizer(BaseSourceCodeSummarizer):
+    _MODEL = "gpt-3.5-turbo"
     _MAX_NUM_REQUESTS_PER_BATCH = 50_000
     _MAX_BYTES_PER_BATCH_FILE = 100_000_000
+    _MAX_TOKEN_COUNT_PER_BATCH = 16_385
+    _MAX_TOKEN_COUNT_PER_RESPONSE = 60
     _client = None
 
     @classmethod
     def initialize(cls) -> None:
         if os.getenv("OPEN_AI_API_KEY") is None:
-            return
+            raise ValueError("OPEN_AI_API_KEY does not exist")
 
         cls._client = openai.OpenAI(api_key=os.environ.get("OPEN_AI_API_KEY"))
 
@@ -29,40 +32,56 @@ class OpenAiSourceCodeSummarizer(BaseSourceCodeSummarizer):
             )
 
     @classmethod
+    def _get_token_count(cls, text: str) -> int:
+        tokenizer = tiktoken.encoding_for_model(cls._MODEL)
+        tokens = tokenizer.encode(text)
+        return len(tokens)
+
+    @classmethod
     def _get_request_batches(cls, source_code_list: list[str]) -> list[list[str]]:
         request_batches = []
 
         request_batch = []
+        batch_token_count = 0
         file_size_bytes = 0
 
         for source_code in source_code_list:
             request = {
-                "custom_id": str(len(request_batch)),
+                "custom_id": str(uuid.uuid4()),
                 "method": "POST",
                 "url": "/v1/chat/completions",
                 "body": {
-                    "model": "gpt-3.5-turbo-0125",
+                    "model": cls._MODEL,
                     "messages": [
                         {
                             "role": "user",
                             "content": f"Semantically summarize this source code into 1 sentence: '{source_code}'",
                         }
                     ],
+                    "max_tokens": cls._MAX_TOKEN_COUNT_PER_RESPONSE,
                 },
             }
 
             request_size_bytes = len(json.dumps(request).encode("utf-8")) + 1
+            request_token_count = (
+                cls._get_token_count(request["body"]["messages"][0]["content"])
+                + cls._MAX_TOKEN_COUNT_PER_RESPONSE
+            )
 
             if (
                 len(request_batch) + 1 > cls._MAX_NUM_REQUESTS_PER_BATCH
                 or file_size_bytes + request_size_bytes > cls._MAX_BYTES_PER_BATCH_FILE
+                or batch_token_count + request_token_count
+                > cls._MAX_TOKEN_COUNT_PER_BATCH
             ):
                 request_batches.append(request_batch)
                 request_batch = []
                 file_size_bytes = 0
+                batch_token_count = 0
 
             request_batch.append(request)
             file_size_bytes += request_size_bytes
+            batch_token_count += request_token_count
 
         if request_batch:
             request_batches.append(request_batch)
@@ -111,15 +130,16 @@ class OpenAiSourceCodeSummarizer(BaseSourceCodeSummarizer):
 
     @classmethod
     def _extract_code_summaries_from_response(
-        cls, request_batch, file_response
+        cls, request_batch, file_response, custom_id_to_index
     ) -> list[str]:
         source_code_summaries = [""] * len(request_batch)
 
         for response in file_response.text.strip().split("\n"):
             response = json.loads(response)
-            source_code_summaries[int(response["custom_id"])] = response["response"][
-                "body"
-            ]["choices"][0]["message"]["content"]
+            source_code_summaries[custom_id_to_index[response["custom_id"]]] = response[
+                "response"
+
+            ]["body"]["choices"][0]["message"]["content"]
 
         return source_code_summaries
 
@@ -127,11 +147,22 @@ class OpenAiSourceCodeSummarizer(BaseSourceCodeSummarizer):
     def _summarize_source_code_list_batch(cls, request_batch: list[str]) -> list[str]:
         batch_input_file = cls._upload_batch_input_file(request_batch)
 
+        custom_id_to_index = {
+            request["custom_id"]: i for i, request in enumerate(request_batch)
+        }
+
         batch = cls._create_batch(batch_input_file)
 
         file_response = cls._client.files.content(batch.output_file_id)
 
-        return cls._extract_code_summaries_from_response(request_batch, file_response)
+        return cls._extract_code_summaries_from_response(
+            request_batch, file_response, custom_id_to_index
+        )
+
+    # @classmethod
+    # def summarize_source_code_list(cls, source_code_list: list[str]) -> list[str]:
+    #     with ThreadPoolExecutor() as executor:
+    #         return list(executor.map(cls._summarize_source_code, source_code_list))
 
     # @classmethod
     # def _summarize_source_code(cls, source_code: str) -> str:
